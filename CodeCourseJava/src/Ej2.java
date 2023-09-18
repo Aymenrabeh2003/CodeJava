71566f941de9d695850ebeae149a3fe180e8ad50
AymenRabehIsHere <alurab6103@ieselcaminas.org>
2023-09-18 11:34:55 +0200

    Guardado1

---
+++ CodeCourseJava/src/Ej2.java
@@ -0,0 +1,12 @@
+public class Ej2 {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b;
+        b = 7;
+        int c = b + a;
+        c+=6; //incrementa el valor de c 6 veces
+        System.out.println(c);
+
+    }
+}
---
+++ CodeCourseJava/src/Ej3.java
@@ -0,0 +1,8 @@
+public class Ej3 {
+    public static void main(String[] args) {
+        final int c = 5;
+
+        System.out.println(c);
+    }
+
+}
