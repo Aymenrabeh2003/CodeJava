9b13eb1000290fe0750823327ca063bcdddfb386
AymenRabehIsHere <alurab6103@ieselcaminas.org>
2023-09-18 11:11:44 +0200

    Guardado1

---
+++ CodeCourseJava/src/Ej1.java
@@ -0,0 +1,11 @@
+public class Ej1 {
+    public static void main(String[] args) {
+
+        // int edad = 35; No siempre se puede usar
+        byte edad;
+        edad = 35;
+
+        System.out.println(edad);
+
+    }
+}
