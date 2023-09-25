d4a39c35fd0bdce5a5e7e7df707aba4cc881dd8c
Aymenrabeh2003 <88269267+Aymenrabeh2003@users.noreply.github.com>
2023-09-25 20:12:13 +0200

    Add files via upload
    GuardadoClase1

--- Miguel/JavaBasico/Ej29.java
+++ Miguel/JavaBasico/Ej29.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Ej29 {
     public static void main(String[] args) {
-
+        //reread
+        int numero;
+        for (numero = 3; numero <= 100; numero++){
+            boolean esPrimo = true;
+                for(int i = 2; i <= Math.sqrt(numero); i++){
+                    if (numero % i == 0) {
+                        esPrimo = false;
+                        break;
+                    }
+                }
+                if (esPrimo){
+                    System.out.println(numero);
+                }
+        }
     }
 }
