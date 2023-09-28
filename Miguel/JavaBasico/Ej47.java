ccbc667d7f24a959ab51691d129efce6ac9a3a21
Aymenrabeh2003 <88269267+Aymenrabeh2003@users.noreply.github.com>
2023-09-28 23:05:04 +0200

    Ej47 actualizado

--- Miguel/JavaBasico/Ej47.java
+++ Miguel/JavaBasico/Ej47.java
@@ -5,19 +5,38 @@ public class Ej47 {
         String [] palabras = new String[5];
         char asterisco = '*';
         Scanner scanner = new Scanner(System.in);
+        int palabraMayor=0;
+        int espacio = 0;
         System.out.println("Escribe 5 palabras y te las pondré en un marco:");
         for (int i = 0; i < 5; i++){
             palabras[i] = scanner.next();
         }
         for (int i = 0; i < 5; i++){
+            if (palabras[i].length() > palabraMayor){
+                palabraMayor = palabras[i].length();
+            }
+        }
+        for (int i = 0; i <= palabraMayor+1; i++){
+            System.out.print(asterisco+"");
+        }
+        System.out.println("");
+        for (int i = 0; i < 5; i++){
             for (int j = 0; j < 5; j++){
-                if (i == 0 || i == 5){
-                    System.out.print(asterisco);
-                }else {
-                    System.out.print(asterisco+""+palabras[i]+""+asterisco);
+                System.out.print("");
+            }
+            if (palabraMayor >= palabras[i].length()){
+                espacio = palabraMayor - palabras[i].length();
+                System.out.print(asterisco+""+palabras[i]+"");
+                for (int f = 0; f < espacio; f++){
+                    System.out.print(" ");
                 }
+                System.out.print(asterisco);
             }
             System.out.println();
         }
+        for (int i = 0; i <= palabraMayor+1; i++){
+            System.out.print(asterisco);
+        }
+
     }
 }
