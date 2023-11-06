6a88a08955948aef58d3f6ee913434889ff29638
Aymenrabeh2003 <88269267+Aymenrabeh2003@users.noreply.github.com>
2023-11-06 20:24:32 +0100

    Add files via upload

--- Miguel/String/23-27/ContarPalabras1.java
+++ Miguel/String/23-27/ContarPalabras1.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class ContarPalabras1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String cad;
+        int count = 0;
+        System.out.println("Escribe un texto y te diré cuantas palabras tiene: ");
+        cad = scanner.nextLine();
+        String[] palabras = Longitud2.dividirEnPalabras(cad);
+
+        for (int i = 0; i < palabras.length; i++){
+            count++;
+        }
+
+        System.out.println("Hay "+ count+ " palabras");
 
     }
+
 }
--- Miguel/String/23-27/ContarPalabras2.java
+++ Miguel/String/23-27/ContarPalabras2.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class ContarPalabras2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String cad;
+        System.out.println("Escribe un texto y te diré cuantas palabras tiene: ");
+        cad = scanner.nextLine();
 
+        System.out.println("Hay "+ ContarPalabras2.contarPalabras(cad)+ " palabras");
+    }
+
+    public static int contarPalabras(String cad){
+        int count = 0;
+        boolean palabra = false;
+        int finLinea = cad.length() - 1;
+
+        for (int i = 0; i < cad.length(); i++){
+            if(Character.isLetter(cad.charAt(i)) && i != finLinea){
+                palabra = true;
+            } else if (!Character.isLetter(cad.charAt(i)) &&
+                    palabra) {
+                count++;
+                palabra = false;
+            } else if(Character.isLetter(cad.charAt(i)) && i == finLinea){
+                count++;
+            }
+        }
+        return count;
     }
 }
