01eaafdd4039724ed6aef2a619b280cdb369d849
Aymenrabeh2003 <88269267+Aymenrabeh2003@users.noreply.github.com>
2023-11-06 19:17:21 +0100

    Add files via upload

--- Miguel/String/18-22/AdivinaVocales.java
+++ Miguel/String/18-22/AdivinaVocales.java
@@ -1,5 +1,89 @@
+import java.util.Scanner;
+
 public class AdivinaVocales {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
+        String cad;
+        String cad2;
+        System.out.println("Escribe una cadena y el siguiente jugador debe adivinar las vocales exactas: ");
+        cad = scanner.nextLine();
+        String cadenaOculta = AdivinaVocales.ocultarVocales(cad);
+        System.out.println("Descifra la cadena: "+ cadenaOculta);
+        cad2 = scanner1.nextLine();
+        if (cad.equals(cad2)){
+            System.out.println("Lo has hecho perfecto");
+        } else {
+            System.out.println("Has hecho un fallo, aqui tienes el resultado correcto: "
+            + cad);
+        }
     }
+/*
+    private static String ocultarVocales(String cad) {
+        String nuevo = "";
+        String vocales = "aeiouAEIOU";
+
+        for (int i = 0; i < vocales.length(); i++){
+            char caracter = cad.charAt(i);
+            if (vocales.indexOf(caracter) != -1){
+                nuevo += '.';
+            } else {
+                nuevo += caracter;
+            }
+        }
+        return nuevo;
+    }
+
+ */
+    private static String ocultarVocales(String cad) {
+
+        cad = cad.replace('a', '.');
+        cad = cad.replace('e', '.');
+        cad = cad.replace('i', '.');
+        cad = cad.replace('o', '.');
+        cad = cad.replace('u', '.');
+
+        cad = cad.replace('A', '.');
+        cad = cad.replace('E', '.');
+        cad = cad.replace('I', '.');
+        cad = cad.replace('O', '.');
+        cad = cad.replace('U', '.');
+
+        return cad;
+    }
+
+
+    /*
+    public static String ocultarVocales(String cad){
+        char[] vocales = new char[]{'a','e','i','o','u'};
+
+        for (int i = 0; i < cad.length(); i++){
+            char charact = cad.charAt(i);
+            for (int j = 0; j < vocales.length; j++){
+                if (charact == vocales[j]) {
+                    charact = '.';
+                    break;
+                }
+            }
+        }
+        return cad;
+    }
+    */
+    /*
+    public static void prueba(){
+        String original = "casa de papel";
+        String nuevo = "";
+        String vocales = "aeiouAEIOU";
+        for (int i=0;i<original.length();i++){
+            if (vocales.contains(""+original.charAt(i))){
+                nuevo = nuevo +".";
+            }else{
+                nuevo = nuevo + original.charAt(i);
+            }
+
+        }
+    }
+
+     */
 }
+
--- Miguel/String/18-22/PalabrasAlfabeticas.java
+++ Miguel/String/18-22/PalabrasAlfabeticas.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class PalabrasAlfabeticas {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String cad;
+        System.out.println("Escribe una palabra y te dire si es o no alfabetica: ");
+        cad = scanner.next();
+        boolean esAlfabetica = PalabrasAlfabeticas.esAlfabetica(cad);
 
+        if (esAlfabetica){
+            System.out.println("La cadena "+cad+" es alfabetica");
+        } else{
+            System.out.println("No es alfabetica");
+        }
+    }
+    public  static boolean esAlfabetica(String palabra){
+        char charactTemp = palabra.charAt(0);
+        for (int i = 0; i < palabra.length(); i++){
+            char numeroChar = palabra.charAt(i);
+
+            if (numeroChar >= charactTemp){
+                charactTemp = numeroChar;
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
     }
 }
